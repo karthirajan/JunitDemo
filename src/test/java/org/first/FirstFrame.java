810b506610d75959866844f24cd558d44eb95a60
Subramaniansan <sanjaysubramanian05@outlook.com>
2021-11-14 11:11:52 +0530

    message

---
+++ src/test/java/org/first/FirstFrame.java
@@ -0,0 +1,11 @@
+package org.first;
+
+public class FirstFrame {
+	
+public static void main(String[] args) {
+	
+	String s = "Frame Work";
+	boolean b = s.contains("Frame");
+	System.out.println(b);
+}
+}
