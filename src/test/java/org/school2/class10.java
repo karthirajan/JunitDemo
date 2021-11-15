802eda19029f85492ef530f224f53316b9011308
sunitasenapati <sunitaaenapati8@gmail.com>
2021-11-15 19:18:34 +0530

    new code added

--- src/test/java/org/school2/class10.java
+++ src/test/java/org/school2/class10.java
@@ -3,6 +3,11 @@ package org.school2;
 public class class10 {
 	public static void main(String[] args) {
 		System.out.println("sunita");
+		System.out.println("Well come to java class");
+		int x=10;
+		int y=20;
+		System.out.println(x+y);
+		
 	}
 
 }
